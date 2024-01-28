0d4a07a8363d92d6920541fe3d479da052723903
Sothyra Chan <juujuu.q.cb@gmail.com>
2024-01-28 14:03:06 -0500

    Logo changed and new files created

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './skills.css';
-import UIDesign from '../../assets/';
-import Webde
+import GUIDesign from '../../assets/gui.png';
+import WebDesign from '../../assets/webdesign.png';
+import AppDesign from '../../assets/DesktopIcon.png';
 
 const Skills = () => {
     return (
         <section id='skills'>
-            
-        </section>
+            <span className="skillTitle">My Skillsets</span>   
+            <span className="skillDesc">I am passionate in learning and exposing my creativity thought creating interactive GUI and web designs. I am proficient in HTML, CSS, and Javascript.</span>
+            <div className="skillBars">
+                <div className="skillBar">
+                    <img src={GUIDesign} alt="GUI" className="skillBarImg" />
+                    <div className="skillBarText">
+                        <h2>GUI Design</h2>
+                        <p>I am able create graphic user interface - GUI using advanced C# language.</p>
+                    </div>
+                </div>
+
+                <div className="skillBar">
+                    <img src={WebDesign} alt="WebDesign" className="skillBarImg" />
+                    <div className="skillBarText">
+                        <h2>Wesite Design</h2>
+                        <p>I can use: HTML, CSS, Javascript, and other frameworks to build websites.</p>
+                    </div>
+                </div>
+
+                <div className="skillBar">
+                    <img src={AppDesign} alt="AppDesign" className="skillBarImg" />
+                    <div className="skillBarText">
+                        <h2>Desktop App Design</h2>
+                        <p>I am skilled in formuted a desktop web form with the use of advanced C#, intermidiate level of mySQL, and LINQ Query. </p>
+                    </div>
+                </div>
+            </div>
+         </section>
     );
 }
