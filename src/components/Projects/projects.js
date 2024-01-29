b77e7c8d7a04e800c475d43564f3bd8f98810344
Sothyra Chan <juujuu.q.cb@gmail.com>
2024-01-28 19:01:20 -0500

    Add some use text elements to the project file

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -8,11 +8,20 @@ const Projects = () => {
     return (
         <section id='projects'>
            <h2 className="projectsTitle">My Projects</h2>
-           <span className="projectsDesc">I always pay attention to the smallest information for all the projects that I have learnt to make personally as well as during in class sessions.<br/>Here are some of my favorite projects captured in images, which I believe have augmented my ability to think outside the box and code.</span>
+           <span className="projectsDesc">I always pay attention to the smallest information for all the individual projects that I have learnt to make personally as well as during in class sessions.<br/>Here are some of my favorite projects captured in images, which I believe have augmented my ability to think outside the box and to code.</span>
            <div className="projectsImgs">
-                <img src={Project1} alt="Project1" className="projectsImg" style={{width: '250px', height: 'auto'}} />
-                <img src={Project2} alt="Project2" className="projectsImg" style={{width: '250px', height: 'auto'}}/>
-                <img src={Project3} alt="Project3" className="projectsImg" style={{width: '250px', height: 'auto'}}/>
+                <div className="projectContainer">
+                    <span className="projectsImgsText"><p1>"Bug Smusher Game"<br/>Users can adjust the speed of the bug <br/> and try to catch it to earn points.</p1></span>
+                        <img src={Project1} alt="Project1" className="projectsImg" style={{width: '300px', height: 'auto'}} />
+                </div>
+                <div className="projectContainer">
+                    <span className="projectsImgsText"><p1>"Personal Portfolio"<br/>This is my personal portfolio project <br/> reflecting my coding experience.</p1></span>
+                        <img src={Project2} alt="Project2" className="projectsImg" style={{width: '300px', height: 'auto'}} />
+                </div>
+                <div className="projectContainer">
+                    <span className="projectsImgsText"><p1>"House Agent Profile"<br/>A profile of an agent with listings <br/> where buyers can view and contact.</p1></span>
+                        <img src={Project3} alt="Project3" className="projectsImg" style={{width: '300px', height: 'auto'}} />
+                </div>
            </div>
         </section>
     );
