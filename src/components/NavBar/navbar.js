b6317603e075fb2e055a21c6713793815f41370b
Sothyra Chan <juujuu.q.cb@gmail.com>
2024-01-28 22:11:37 -0500

    add new icon menu bar

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 // import logo from '../../assets/logo.png';
 import {Link} from 'react-scroll';
-import contactImg from '../../assets/contact.png'
+import contactImg from '../../assets/contact.png';
+import menuIcon from '../../assets/menu.png';
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
     return (
         <nav className="navbar">
             <img src="logo.png" alt="logo" className='logo'/>
@@ -19,6 +21,15 @@ const Navbar = () => {
             }
         }>
                 <img src={contactImg} alt="" className="desktopMenuImg" />Contact Me</buttom>
+
+            <img src="menuIcon.png" alt="Menu" className='mobileMenu' style={{width: '60px', height: 'auto'}} onClick={() =>setShowMenu(!showMenu)}/>
+            <div className="navMenu" style={{display: showMenu? 'flex':'none'}}>
+                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="listItem"   onClick={() =>setShowMenu(false)}>Home</Link>
+                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className="listItem"  onClick={() =>setShowMenu(false)}>About</Link>
+                <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-100} duration={500} className="listItem"onClick={() =>setShowMenu(false)}>Project</Link>
+                <Link activeClass='active' to='services' spy={true} smooth={true} offset={-120} duration={500} className="listItem"onClick={() =>setShowMenu(false)}>Service</Link>
+                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() =>setShowMenu(false)}>Contact</Link>
+            </div>
         </nav>
     )
 }
